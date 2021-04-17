cbaccf26491cbec75e9c895981d67d656bd030dc
AyushiPanth <lit2019073@iiitl.ac.in>
2021-04-18 02:41:33 +0530

    landing on homepage

--- hackfiles/lib/main.dart
+++ hackfiles/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: Dashboard(),
+      home: WelcomePage(),
     );
   }
 }
