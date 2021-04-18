b6f7ff7a951093f00b1c9d97ba6b027488c721dd
Gautam Patodi <gpatodi15@gmail.com>
2021-04-18 16:53:42 +0530

    issue #18 fixed

--- hackfiles/lib/profile_ui.dart
+++ hackfiles/lib/profile_ui.dart
@@ -1,6 +1,7 @@
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/screenutil.dart';
+import 'package:hackfiles/welcomepage.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -209,7 +210,9 @@ class ProfileScreen extends StatelessWidget {
                       ProfileListItem(
                         icon: LineAwesomeIcons.alternate_sign_out,
                         text: 'Logout',
-                        hasNavigation: false,
+                        hasNavigation: true,
+                        fun: () => Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (_) => WelcomePage())),
                       ),
                     ],
                   ),
@@ -227,50 +230,55 @@ class ProfileListItem extends StatelessWidget {
   final IconData icon;
   final String text;
   final bool hasNavigation;
+  final Function fun;
 
   const ProfileListItem({
     Key key,
     this.icon,
     this.text,
     this.hasNavigation = true,
+    this.fun = null,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: kSpacingUnit.w * 5.5,
-      margin: EdgeInsets.symmetric(
-        horizontal: kSpacingUnit.w * 4,
-      ).copyWith(
-        bottom: kSpacingUnit.w * 2,
-      ),
-      padding: EdgeInsets.symmetric(
-        horizontal: kSpacingUnit.w * 2,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(kSpacingUnit.w * 3),
-        color: Theme.of(context).backgroundColor,
-      ),
-      child: Row(
-        children: <Widget>[
-          Icon(
-            this.icon,
-            size: kSpacingUnit.w * 2.5,
-          ),
-          SizedBox(width: kSpacingUnit.w * 1.5),
-          Text(
-            this.text,
-            style: kTitleTextStyle.copyWith(
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          Spacer(),
-          if (this.hasNavigation)
+    return GestureDetector(
+      onTap: fun,
+      child: Container(
+        height: kSpacingUnit.w * 5.5,
+        margin: EdgeInsets.symmetric(
+          horizontal: kSpacingUnit.w * 4,
+        ).copyWith(
+          bottom: kSpacingUnit.w * 2,
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: kSpacingUnit.w * 2,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(kSpacingUnit.w * 3),
+          color: Theme.of(context).backgroundColor,
+        ),
+        child: Row(
+          children: <Widget>[
             Icon(
-              LineAwesomeIcons.angle_right,
+              this.icon,
               size: kSpacingUnit.w * 2.5,
             ),
-        ],
+            SizedBox(width: kSpacingUnit.w * 1.5),
+            Text(
+              this.text,
+              style: kTitleTextStyle.copyWith(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Spacer(),
+            if (this.hasNavigation)
+              Icon(
+                LineAwesomeIcons.angle_right,
+                size: kSpacingUnit.w * 2.5,
+              ),
+          ],
+        ),
       ),
     );
   }
